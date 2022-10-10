0f4a942b83bcac034c79baf6a5ada6ac75fa8cc0
jmartinelena <jmartinelena1@gmail.com>
2022-10-10 17:00:55 -0300

    Agrego consumicion de APIs
    Consumo la API de random.dog tanto con .NET usando HttpWebRequest y con jQuery usando ajax. Modifico la view de las paginas y quito la API que viene por defecto al crear el proyecto.

---
+++ Practica.EF/Practica.MVC.API/Models/RandomDogResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica.MVC.API.Models
+{
+    public class RandomDogResponse
+    {
+        public int FileSizeBytes { get; set; }
+        public string Url { get; set; }
+    }
+}
